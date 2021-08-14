abfc8590804c3144ca257ea2f7bec7c4f0aa988b
SkySarDev <skysar@ya.ru>
2021-08-14 16:25:11 +0300

    Feat: add popup-gift (task #05)

--- src/js/modules/handlers.js
+++ src/js/modules/handlers.js
@@ -1,12 +1,28 @@
 import popups from "./popups";
 
 const handlers = () => {
+  let showPopupGift = true;
+
+  window.addEventListener("scroll", () => {
+    if (
+      showPopupGift &&
+      window.pageYOffset + document.documentElement.clientHeight >=
+        document.documentElement.scrollHeight
+    ) {
+      showPopupGift = false;
+      popups(document.querySelector(".popup-gift"), true);
+      document.querySelector(".fixed-gift").style.display = "none";
+    }
+  });
+
   document.addEventListener("click", (e) => {
     const target = e.target;
 
     // Show modals
     if (target.getAttribute("data-show-modal")) {
       const popupSel = `.${target.getAttribute("data-show-modal")}`;
+
+      showPopupGift = false;
       popups(document.querySelector(popupSel), true);
 
       if (target.classList.contains("fixed-gift")) {
@@ -19,6 +35,7 @@ const handlers = () => {
       target.classList.contains("popup-close") ||
       target.classList.contains("popup")
     ) {
+      showPopupGift = false;
       popups(target.closest("[data-modal]"), false);
     }
   });
