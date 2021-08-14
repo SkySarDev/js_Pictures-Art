22abb24e17a90648eda2525f4ce0b3da06fb8827
SkySarDev <skysar@ya.ru>
2021-08-14 15:58:05 +0300

    Chore: hide scroll when popup is open

--- src/js/modules/popups.js
+++ src/js/modules/popups.js
@@ -1,7 +1,11 @@
+import { scrollHide, scrollShow } from "./services";
+
 const popups = (popup, show) => {
   if (show) {
+    scrollHide();
     popup.classList.add("show-popup");
   } else {
+    scrollShow();
     popup.classList.remove("show-popup");
   }
 };
